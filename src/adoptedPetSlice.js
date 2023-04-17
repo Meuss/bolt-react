5814808215be6134ee6fa4a48e112694bd974474
Thomas Miller <thomas.miller147@gmail.com>
2023-04-17 20:30:27 +0200

    redux basics

---
+++ src/adoptedPetSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const adoptedPetSlice = createSlice({
+  name: "adoptedPet",
+  initialState: {
+    value: null,
+  },
+  // function that takes the current state and an action object
+  // and returns a new state based on that action
+  reducers: {
+    adopt: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { adopt } = adoptedPetSlice.actions;
+export default adoptedPetSlice.reducer;
